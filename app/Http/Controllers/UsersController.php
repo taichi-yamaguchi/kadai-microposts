789b002d2ce8ba2a08e0e8c13d2b1e21460ed9d8
taichi.yamagu <taizoyama44@gmail.com>
2020-09-12 02:04:31 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,16 +18,7 @@ public function index()
 
 }
 
-public function show($id)
-    {
-        // idの値でユーザを検索して取得
-        $user = User::findOrFail($id);
 
-        // ユーザ詳細ビューでそれを表示
-        return view('users.show', [
-            'user' => $user,
-        ]);
-    }
 public function show($id)
     {
         // idの値でユーザを検索して取得
@@ -43,6 +34,47 @@ public function show($id)
         return view('users.show', [
             'user' => $user,
             'microposts' => $microposts,
+        ]);
+    }
+     public function followings($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+
+        // フォロー一覧ビューでそれらを表示
+        return view('users.followings', [
+            'user' => $user,
+            'users' => $followings,
+        ]);
+    }
+
+    /**
+     * ユーザのフォロワー一覧ページを表示するアクション。
+     *
+     * @param  $id  ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function followers($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロワー一覧を取得
+        $followers = $user->followers()->paginate(10);
+
+        // フォロワー一覧ビューでそれらを表示
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
         ]);
     }
     
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,15 +3,9 @@
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ $user->name }}</h3>
-                </div>
-                <div class="card-body">
-                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
-                </div>
-            </div>
+            {{-- ユーザ情報 --}}
+        @include('users.card')
+            
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
@@ -23,10 +17,22 @@
                     </a>
                 </li>
                 {{-- フォロー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
+                <li class="nav-item">
+                    <a href="{{ route('users.followings', ['id' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.followings') ? 'active' : '' }}">
+                        Followings
+                        <span class="badge badge-secondary">{{ $user->followings_count }}</span>
+                    </a>
+                </li>
                 {{-- フォロワー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+                <li class="nav-item">
+                    <a href="{{ route('users.followers', ['id' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.followers') ? 'active' : '' }}">
+                        Followers
+                        <span class="badge badge-secondary">{{ $user->followers_count }}</span>
+                    </a>
+                </li>
+
             </ul>
+            
             @if (Auth::id() == $user->id)
                 {{-- 投稿フォーム --}}
                 @include('microposts.form')
